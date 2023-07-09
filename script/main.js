b9c60143554607f2dae24cdc26c2cdc04173e723
Rishab49 <rishab.rishu49@gmail.com>
2023-07-09 11:11:34 +0530

    added features and made responsive

--- script/main.js
+++ script/main.js
@@ -2,3 +2,15 @@
 let array;
 let mainInterval;
 let numberOfElements = 20;
+let isOpen = false;
+let factor;
+
+let graph_container = document.querySelector(".graph_container");
+let randomButton = document.querySelector("#randomButton");
+let runButton = document.querySelector(".run_button");
+let form = document.querySelector("form");
+let notificationElement = document.querySelector(".notification");
+let toggleButton = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+
+factor = graph_container.getBoundingClientRect().width / 21;
